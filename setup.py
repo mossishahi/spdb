6bf6ce12a40f21238629fb7caf56433962059083
mossishahi <ge65cew@mytum.de>
2025-10-07 18:30:36 +0200

    repo init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='spdb',
+      version='0.1.0',
+      description='spdb',
+      author='Mostafa Shahhoseini',
+      author_email='shahhosseini.94@gmail.com',
+      url='https://github.com/spdb.git',
+      packages=['spdb'],
+     )
