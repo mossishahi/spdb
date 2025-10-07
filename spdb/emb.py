c6d87f7967b4079b576ec10e4e607436111486bc
mossishahi <ge65cew@mytum.de>
2025-10-07 22:51:45 +0200

    wrapper implemented for embedding function

--- spdb/emb.py
+++ spdb/emb.py
@@ -1,10 +1,11 @@
 from spdb.utils._keys import OPENAI_API_KEY
 import os
 from openai import OpenAI
+from spdb.abs import get_abstract
 
 def get_embedding(text: str,
-    model: str = "text-embedding-3-small"
-    ):
+                model: str = "text-embedding-3-small"
+                ):
     """
     Get an embedding for a given text.
     """
@@ -15,3 +16,11 @@ def get_embedding(text: str,
     )
     embedding_vector = response.data[0].embedding
     return embedding_vector
+
+def url_to_embedding(url: str):
+    """
+    Get an embedding for a given text.
+    """
+    abstract = get_abstract(url)
+    return get_embedding(abstract)
+
